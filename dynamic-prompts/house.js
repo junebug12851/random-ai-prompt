5068be55a3c2ae6491ad5dc2b43422b6adf57dfe
junebug12851 <junebug12851@users.noreply.github.co>
2022-12-07 18:49:39 -0500

    Added house dynamic prompt

---
+++ dynamic-prompts/house.js
@@ -0,0 +1,80 @@
+/*
+    Copyright 2022 juenbug12851
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+const _ = require("lodash");
+
+function multiColor() {
+	return (_.random(0.0, 1.0, true) < 0.5) ? `multi color ` : ``;
+}
+
+module.exports = function(prompt, settings, imageSettings, upscaleSettings) {
+
+	// Start with base prompt
+	prompt = "house";
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", {building-style}"
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", {time}"
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += `, ${multiColor()}{flower}`
+	
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += `, ${multiColor()}{flower}`
+	
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += `, ${multiColor()}vegetation`
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += `, ${multiColor()}{tree}`
+	
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += `, ${multiColor()}{tree}`
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", vines";
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", {weather}"
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", {weather}"
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", {render-color}";
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", {render-color}";
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", <rays>";
+
+	prompt == ", lense flares";
+
+	if(_.random(0.0, 1.0, true) < 0.5)
+		prompt += ", {art-style}";
+
+	const artistCount = (settings.includeArtist) ? _.random(1, 3, false) : 0;	
+
+	// Add in artist
+	for(let i = 0; i < artistCount; i++) {
+		prompt += ", {artist}";
+	}
+
+	return prompt;
+}
