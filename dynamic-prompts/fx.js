a127f7d26dce26f1f1a9a8afe8975ba109ac6abd
junebug12851 <junebug12851@users.noreply.github.co>
2023-01-03 13:02:02 -0500

    Make <candlelight> more rare

--- dynamic-prompts/fx.js
+++ dynamic-prompts/fx.js
@@ -20,7 +20,7 @@ module.exports = function() {
 
 	let prompt = "";
 
-	if(_.random(0.0, 1.0, true) < 0.1)
+	if(_.random(0.0, 1.0, true) < 0.05)
 		prompt += ", <candlelight>";
 
 	if(_.random(0.0, 1.0, true) < 0.5)
