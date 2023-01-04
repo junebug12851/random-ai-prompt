fa5e28a612f1486601f13a7aa39edbc12b64a05c
junebug12851 <junebug12851@users.noreply.github.co>
2023-01-04 04:12:43 -0500

    Added new dynamic prompt, vibrant-art

---
+++ dynamic-prompts/vibrant-art.js
@@ -0,0 +1,43 @@
+/*
+    Copyright 2022 juenbug12851
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+const _ = require("lodash");
+
+function colorful() {
+    let colorful = "colorful";
+
+    if(_.random(0.0, 1.0, false) < 0.5)
+        colorful += " multi-color";
+
+    if(_.random(0.0, 1.0, false) < 0.5)
+        colorful += " glow";
+
+    return colorful;
+}
+
+module.exports = function(settings) {
+
+    // This will not work well with added artists or fx
+    settings.autoAddArtists = false;
+    settings.autoAddFx = false;
+
+	// Start with base prompt
+	let prompt = `${colorful()} {flower}, ${colorful()} {animal}, (black background), (black paper), (ink outlines), very detailed`;
+
+	return prompt;
+}
+
+module.exports.full = true;
