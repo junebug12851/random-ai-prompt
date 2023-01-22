ae1b6b1a6a07ec9329c1b519f26eecfb1f86fba0
junebug12851 <junebug12851@users.noreply.github.co>
2023-01-22 09:51:03 -0500

    removed shed type from cabin

--- dynamic-prompts/log-cabin.js
+++ dynamic-prompts/log-cabin.js
@@ -21,9 +21,6 @@ module.exports = function() {
     // Start with base prompt
     let prompt = "((cabin, log cabin)), dense forest, dense woods, dense trees";
 
-    if(_.random(0.0, 1.0, true) < 0.35)
-        prompt += ", {shed-type}"
-
     if(_.random(0.0, 1.0, true) < 0.5)
         prompt += ", fog"
 
